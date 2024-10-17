a8f5d910c2c17ec1d739025234bcc0160aa583c0
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2024-10-17 21:10:05 +0500

    add auth

---
+++ components/auth/index.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import React from "react";
+import Button from "../ui/Button";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+
+const Auth = () => {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen">
+        <Image
+          src={"/images/x.svg"}
+          alt="X"
+          width={450}
+          height={450}
+          className="justify-self-center hidden md:block"
+        />
+
+        <div className="flex flex-col justify-center md:justify-between h-full md:h-[70vh]">
+          <div className="block md:hidden">
+            <Image src={"/images/x.svg"} alt="X" width={50} height={50} />
+          </div>
+          <h1 className="text-5xl font-bold mb-4">Happening now</h1>
+          <div className="w-full md:w-[60%]">
+            <h2 className="font-bold text-3xl mb-4">Join today.</h2>
+            <div className="flex flex-col space-y-3">
+              <Button
+                label={
+                  <div className="flex items-center gap-2 justify-center">
+                    <FcGoogle />
+                    Sign up with google
+                  </div>
+                }
+                fullWidth
+                secondary
+              />
+              <Button
+                label={
+                  <div className="flex items-center gap-2 justify-center">
+                    <AiFillGithub />
+                    Sign up with github
+                  </div>
+                }
+                fullWidth
+                secondary
+              />
+              <div className="flex items-center justify-center">
+                <div className="h-px bg-gray-700 w-1/2" />
+                <p className="mx-4">or</p>
+                <div className="h-px bg-gray-700 w-1/2" />
+              </div>
+              <Button label={"Create account"} fullWidth />
+            </div>
+          </div>
+          <div className="w-full lg:w-[60%] mt-5">
+            <h3 className="font-meium text-xl mb-4">Already have an account</h3>
+            <Button label={"Signin"} fullWidth outline />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
---
+++ components/ui/Button.tsx
@@ -0,0 +1,45 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  label: ReactNode | string;
+  secondary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  disabled?: boolean;
+  outline?: boolean;
+  type?: "button" | "submit";
+  onClick?: () => void;
+}
+
+const Button = ({
+  label,
+  disabled,
+  fullWidth,
+  large,
+  onClick,
+  outline,
+  secondary,
+  type,
+}: ButtonProps) => {
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className={cn(
+        "rounded-full font-semibold border transition hover:opacity-80 disabled:opacity-70 disabled:cursor-not-allowed",
+        fullWidth ? "w-full" : "w-fit",
+        secondary ? "bg-white text-black" : "bg-sky-500 text-white",
+        large ? "text-xl px-5 py-3" : "text-md px-4 py-3",
+        outline
+          ? "bg-transparent border-slate-600 text-sky-500 hover:bg-slate-800/40"
+          : ""
+      )}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
